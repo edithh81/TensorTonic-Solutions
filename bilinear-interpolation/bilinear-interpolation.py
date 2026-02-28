19a9ad48c7d1110d19a0343696a8d23aedff31ec
Edithhh <124547125+edithh81@users.noreply.github.com>
2026-02-28 17:39:25 +0700

    Update solution for bilinear-interpolation

--- bilinear-interpolation/bilinear-interpolation.py
+++ bilinear-interpolation/bilinear-interpolation.py
@@ -4,28 +4,37 @@ def bilinear_resize(image, new_h, new_w):
     Resize a 2D grid using bilinear interpolation.
     """
     # Write code here
-    image = np.asarray(image)
+    image = np.asarray(image, dtype = np.float64)
     H, W = image.shape
-    image_out = np.zeros((new_h, new_w))
+
+    src_y = np.zeros(new_h) if new_h == 1 else \
+        np.linspace(0, H-1, new_h)
+    src_x = np.zeros(new_w) if new_w == 1 else\
+        np.linspace(0, W-1, new_w)
+
+    y0 = np.floor(src_y).astype(int)
+    x0 = np.floor(src_x).astype(int)
+
+    y1 = np.clip(y0 + 1, 0, H-1)
+    x1 = np.clip(x0 + 1, 0, W-1)
     
-    h_scale = (H-1) / (new_h-1) if new_h != 1 else 0
-    w_scale = (W-1) / (new_w -1) if new_w != 1 else 0
+    dy = src_y - y0
+    dx = src_x - x0
+
+    y0 = y0[:, None]
+    y1 = y1[:, None]
+    dy = dy[:, None]
+
+    x0 = x0[None, :]
+    x1 = x1[None, :]
+    dx = dx[None, :]
+
+    Ia = image[y0, x0]
+    Ib = image[y0, x1]
+    Ic = image[y1, x0]
+    Id = image[y1, x1]
+    top = Ia * (1 - dx) + Ib * dx
+    bottom = Ic * (1-dx) + Id * dx
+    image_out = top * (1-dy) + bottom * dy
+    return image_out.tolist()
     
-    for i in range(new_h):
-        src_y = i * h_scale
-        y0 = int(np.floor(src_y))
-        y1 = min(y0 + 1, H-1)
-        dy = src_y - y0
-        for j in range(new_w):
-            src_x = j * w_scale
-            x0 = int(np.floor(src_x))
-            x1 = min(x0+1, W-1)
-            dx = src_x - x0
-            top = image[y0][x0] * (1-dx) + image[y0][x1] * dx
-            bottom = image[y1][x0] * (1-dx) + image[y1][x1] * dx
-            res = top * (1-dy) + bottom * dy
-            image_out[i][j] = res
-        
-    return image_out.astype(np.float32).tolist()
-            
-            
